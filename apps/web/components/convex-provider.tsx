5ff256e50b2fd10d60c5a5ccbf146989998f79aa
SATADRU1 <satadrumondal1331@gmail.com>
2026-04-13 11:36:17 +0530

    feat: add convex as my database server

---
+++ apps/web/components/convex-provider.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import * as React from "react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "");
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+}
