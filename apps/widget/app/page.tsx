2bca0bde169c67aa6956f22c4144bcb337e4291d
SATADRU1 <satadrumondal1331@gmail.com>
2026-04-21 12:54:03 +0530

    Feat : ai voice assistant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,25 +1,29 @@
 "use client";
 
-import { useQuery, useMutation } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
+
 import { Button } from "@workspace/ui/components/button";
+import { useVapi } from "@/modules/widget/hooks/use-vapi";
 
 export default function Page() {
 
+  const {startCall, stopCall, isConnected, isConnecting, isSpeaking, transcript} = useVapi();
 
-
-
-  const users = useQuery(api.user.getMany);
-  const addUser = useMutation(api.user.add);
   
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-     <p>apps/widget</p>
-     <Button onClick={() => addUser({})}>Add User</Button>
-     <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-     </div> 
+
+     <Button onClick={() => startCall()}>
+      Start call
+     </Button>
+
+     <Button onClick={() => stopCall()} variant={"destructive"}>
+      Stop call
+     </Button>
+     <p>isConnected: {`${isConnected}`}</p>
+     <p>isConnecting: {`${isConnecting}`}</p>
+     <p>isSpeaking: {`${isSpeaking}`}</p>
+     <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   )
 }
